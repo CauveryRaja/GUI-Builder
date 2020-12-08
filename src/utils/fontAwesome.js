2ffa0069a5f40aed831ec60cb6084f97419fc0c4
CauveryRaja <cauveryraja25@gmail.com>
2020-12-08 16:29:37 +0530

    Removed Commented Imports in FontAwesome library

--- src/utils/fontAwesome.js
+++ src/utils/fontAwesome.js
@@ -4,10 +4,6 @@ import { faPlus, faEdit, faTrash, faICursor, faSquare, faParagraph,
         faHeading, faFont, faThList, faCog, faFile, faPaintBrush }
         from '@fortawesome/free-solid-svg-icons';
 
-// import { faHeader }
-//         from '@fortawesome/free-regular-svg-icons';
-
-
 library.add(
   faPlus, faEdit, faTrash, faICursor, faSquare, faParagraph, faHeading,
   faFont, faThList, faCog, faFile, faPaintBrush
